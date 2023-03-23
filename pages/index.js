20e96060710da575b1f77caed5993dd17c50d6c5
jsolvaag <128585674+jsolvaag@users.noreply.github.com>
2023-03-23 08:39:18 +0000

    test

---
+++ pages/index.js
@@ -0,0 +1,69 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      
+        <h1 className={styles.title}>
+          Welcome to <a href="https://www.google.com/search?q=you+have+been+micald&rlz=1C5GCEM_en&oq=you+have+been+micald&aqs=chrome..69i57j33i22i29i30l9.7811j0j1&sourceid=chrome&ie=UTF-8">mical jakson!</a> 
+        </h1> target=
+
+        
+        
+
+
+                    
+                   
+          
+
+        <div className={styles.grid}>
+          <a href="https://en.wikipedia.org/wiki/Death_of_Michael_Jackson" target="_blank" className={styles.card}>
+            <h3>micaljakson death
+               &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://www.thesouljam.com/post/michael-jackson-lessons" target="_blank" className={styles.card}>
+            <h3>Learn about micaljakson&rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href= "https://www.dbltap.com/posts/michael-jackson-fortnite-the-king-of-pop-imagined-in-game-01evf9fm1ejr" target="_blank"
+            className={styles.card}
+          >
+            <h3>Examples of micaljakson skins in the new call of duti &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://www.google.com/search?q=call+of+duty+michael+jackson+deploy&source=lmns&bih=946&biw=1680&rlz=1C5GCEM_en&hl=en&sa=X&ved=2ahUKEwiw5d2Fw-_9AhUJzyoKHb38AYsQ_AUoAHoECAEQ" target="_blank"
+            className={styles.card}
+          >
+            <h3>Deploy micaljakson in the new call of duti 2 &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+      
+
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+        </a>
+      </footer>
+      </div>
+  )
+}
